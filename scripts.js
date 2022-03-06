09fd3ced8943e381ea61ab99f69ce385d7b68323
sungu <jcliffland@gmail.com>
2022-03-06 12:13:41 +0300

    added day of week formula

--- scripts.js
+++ scripts.js
@@ -49,5 +49,9 @@ function getAkanName(){
             return true;
         }
     }
+    var monthValid = monthValidator = monthValidator();
+    var dayValid = dayValidator();
+    var dayOfTheWeek;
     
+    var dayOfTheWeek = Math.floor((((Number(yearOfBirth.slice(0,2))/4)-2 * Number(yearOfBirth.slice(0,2))- 1) +((5 * Number(yearOfBirth.slice(2,4))/ 4))+ ((26 * (monthOfBirth + 1) / 10)) + dayOfBirth) % 7)
 }
